717786f6dc0803d589763581eb5e84919728adbd
Josh Cummings <josh@madebygrizzly.com>
2017-09-14 16:27:23 -0700

    [fix](variables.sh): updates demo vars to match circle.yml

--- bin/vars/variables.sh
+++ bin/vars/variables.sh
@@ -2,11 +2,8 @@
 
 WP_CONTAINER=awp_demo_wordpress_1
 MYSQL_CONTAINER=awp_demo_mysql_1
+
 STG_IP=127.0.0.1
-STG_DOMAIN=staging.test.com
-STG_USER=josh
+STG_DOMAIN=example.com
+STG_USER=admin
 STG_DB=testdb
-PROD_IP=127.0.0.1
-PROD_DOMAIN=test.com
-PROD_USER=josh
-PROD_DB=testdb
